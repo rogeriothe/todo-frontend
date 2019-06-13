a52929b792da3cf99fd820d1d688a0d76a00be42
Rogério de Lima Sousa <rogeriothe@gmail.com>
2019-06-13 18:17:37 -0300

    aula 65

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://web.unifsa.com.br:3003/api/todos'
 
 export default class Todo extends Component {
 
@@ -19,7 +22,9 @@ export default class Todo extends Component {
     }
 
     handleAdd() {
-        console.log(this)
+        const description = this.state.description
+        axios.post(URL, { description })
+            .then(resp => console.log('Funfou!'))
     }
 
     render() {
