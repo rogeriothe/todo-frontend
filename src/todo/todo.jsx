12b4d6a0d65c8c58f437fb58ff7eb77da5c79ea5
Rogério de Lima Sousa <rogeriothe@gmail.com>
2019-06-13 18:35:04 -0300

    aula 66

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -15,7 +15,16 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
     }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({ ...this.state, description: '', list: resp.data }))
+    }
+
     handleChange(e) {
         this.setState({ ...this.state, description: e.target.value })
 
@@ -24,9 +33,13 @@ export default class Todo extends Component {
     handleAdd() {
         const description = this.state.description
         axios.post(URL, { description })
-            .then(resp => console.log('Funfou!'))
+            .then(resp => this.refresh())
     }
 
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
     render() {
         return (
             <div>
@@ -34,7 +47,7 @@ export default class Todo extends Component {
                 <TodoForm description={this.state.description}
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd} />
-                <TodoList />
+                <TodoList list={this.state.list} handleRemove={this.handleRemove} />
             </div>
 
         )
