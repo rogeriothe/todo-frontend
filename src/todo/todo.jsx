25ca5c7e5c62998a753d994802bdc8915b76997e
Rogério de Lima Sousa <rogeriothe@gmail.com>
2019-06-13 17:37:41 -0300

    aula 64

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -6,6 +6,18 @@ import TodoList from './todoList'
 
 export default class Todo extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value })
+
+    }
+
     handleAdd() {
         console.log(this)
     }
@@ -14,9 +26,12 @@ export default class Todo extends Component {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm handleAdd={this.handleAdd} />
+                <TodoForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
+
         )
     }
 }
