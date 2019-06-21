955520505c008392a06e9b5661c05d04e9e54dd0
Rogério de Lima Sousa <rogeriothe@gmail.com>
2019-06-21 16:16:02 -0300

    house

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -14,8 +14,10 @@ export default props => (
         <Grid cols='12 3 2'>
             <IconButton style='primary' icon='plus'
                 onClick={props.handleAdd} ></IconButton>
+            <IconButton style='info' icon='search'
+                onClick={props.handleSearch}></IconButton>
         </Grid>
 
-        
+
     </div>
 )
